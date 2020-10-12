cc438ddfc261949be474d07414407d01679594fa
Smos Bois <simondeb1505@gmail.com>
2020-10-12 23:07:22 +0200

    Added grabscript and base plan for tele and turning

---
+++ VR Experiment/Assets/Scripts/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //shoot raycast at ground
+        //Check if the distance is not too close to the player
+        //If the distance is far enough, show teleport option
+        //It the player presses the teleport button, move his position
+        //Maybe add a way that it doesn't feel really weird (we don't want it to be instant or people will barf)
+    }
+}
---
+++ VR Experiment/Assets/Scripts/Turn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //turn the controller stick to the right and turn right (same with left)
+        //Snap turns are better for motion sickness
+        //Don't forget to do it only once until the player releases the stick
+        //otherwise they'll be turning at lightspeed
+    }
+}
